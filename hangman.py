53043eee060b175988873056fab31c5a864124a9
Samuel Taylor <github@samueltaylor.org>
2018-11-01 20:43:46 +0000

    Improve comments

--- hangman.py
+++ hangman.py
@@ -1,14 +1,9 @@
-
-
-
-# This is an ARRAY of the body parts of the man that will appear when a wrong guess is entered
+# list of the body parts that will appear when a wrong guess is entered
 hangman_parts = [ "head", "left arm", "torso", "right arm", "left leg", "right leg" ]
 
-# This variable sets the number of allowed guesses
-# Notice we used the len function to COUNT the number of items in the array/list
-num_wrong_guesses_allowed = len(hangman_parts)
+num_wrong_guesses_allowed = len(hangman_parts)  # notice: len() gets us the length of the list
 
-# This is setting words that can be used for guesses...in an ARRAY
+# words that our program chooses between to guess
 words = [
     "apple",
     "butterfly",
@@ -28,13 +23,13 @@ words = [
     "weather",
     "twist"
     ]
-# this function will draw based on the number of wrong guesses
+
+# convenience function provided to 
 def draw_hangman(num_wrong_guesses):
     if num_wrong_guesses > num_wrong_guesses_allowed:
         # increment the count if wrong guess
         num_wrong_guesses = num_wrong_guesses_allowed
 
-# this is a DICTIONARY of the characters used for the body parts NOTE '{' and not '['
     hangman_characters = {
         "head" : "  O",
         "left arm" : " /",
@@ -57,27 +52,23 @@ def draw_hangman(num_wrong_guesses):
     output = output + "____\n\n"
     print(output)
 
-# Below using an input funtion to prompt the player for his/her name
+# prompt player for her/his name
 name = input("What is your name? ")
 
-# Print hello and the name entered (basically printing a string of text WITH name by concatenating
-print("Hello " + name + " lets play the hangman")
+print("Hello", name, " let's play Hangman!")
 
-#set the counter for your loop
+# need to track the number of incorrect guesses made so far
 wrong_guesses = 0
+word = 'apple'
 
-# the variable used below named num_wrong_guesses_allowed is a count of the body parts...if you added additional body parts later, your guesses would increase automatically
 while wrong_guesses < num_wrong_guesses_allowed:
-    # using input() and lower() the lower function is used incase a guess is UPPERCASE
-    guess =input("What is your guess? ").lower()
-    if guess in words:
+    guess = input("What is your guess? ").lower()  # .lower()
+    if guess in word:
         print("Correct")
     else:
         print("Wrong")
-        # below is your counter for the guesses...everytime an incorrect guess is entered, you need to increase the guess count by ONE
-        wrong_guesses=wrong_guesses + 1
-    # below is teh function that will draw the man based on the number of guesses made
+        wrong_guesses += 1
     draw_hangman(wrong_guesses)
-    #the statement below uses logic to see if the nnumber of guess and guess allowed has been met, if so than game over!
+
 if wrong_guesses == num_wrong_guesses_allowed:
-    print ("Sorry, You Lost, game over")
+    print("GAME OVER")
